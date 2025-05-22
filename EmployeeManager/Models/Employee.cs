3976c94780287203e915e2621d0e7e5a4a40ac58
Denis Pokhodnia <denispokhodnia@gmail.com>
2025-05-22 13:10:48 +0300

    add entity Employee

---
+++ EmployeeManager/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Position { get; set; }      
+        public int BirthYear { get; set; }        
+        public decimal Salary { get; set; }
+
+    }
+}
