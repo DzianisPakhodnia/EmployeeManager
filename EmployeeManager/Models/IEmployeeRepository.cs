288ef7fb891eea92402745ab977a549aefca2b79
Denis Pokhodnia <denispokhodnia@gmail.com>
2025-05-22 13:35:12 +0300

    add IEmployeeRepository and EmployeeRepository

---
+++ EmployeeManager/Models/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models
+{
+    public interface IEmployeeRepository
+    {
+        void AddEmployee(Employee employee);
+        void DeleteEmployee(Employee employee);
+
+        IEnumerable<Employee> GetAllEmployees();
+        IEnumerable<Employee> GetEmployeesByPosition(string position);
+    }
+}
---
+++ EmployeeManager/Repositories/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetEmployeesByPosition(string position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
